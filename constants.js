55e15b68e73fe917a7412bf61ea6a269903db6e8
KaushikTest <kaushik6990@gmail.com>
2025-08-19 05:43:42 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,2 +1,13 @@
 
 export const TIME_FORMAT = "yyyy-LL-dd'T'HH:mm:ssZZ";
+export const MISSING_FIELD = 'Missing required fields.';
+export const INVALID_DATE = 'Invalid date format.';
+export const TIME_ERROR = 'Start time must be before end time.';
+export const OVERLAP_ERROR = 'Event overlaps with an existing event.';
+export const EVENT_BOOKED = 'Event booked successfully.';
+export const ID_ERROR = 'Event id is required.';
+export const EVENT_ERROR = 'Event not found.';
+export const EVENT_BLOCKED = 'Event blocked successfully.';
+export const EVENT_RELEASED = 'Event released successfully.';
+export const EVENT_UPDATED = 'Event updated successfully.';
+export const EVENT_DELETED = 'Event deleted successfully.'
