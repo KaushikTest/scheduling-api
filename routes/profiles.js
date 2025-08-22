be889b837864f7ea9f42b3dc27130f54efed8fd1
KaushikTest <kaushik6990@gmail.com>
2025-08-22 08:29:30 +0530

    Update profiles.js

--- routes/profiles.js
+++ routes/profiles.js
@@ -13,6 +13,18 @@ profileRouter.post('/create', (req, res) => {
     const insert = db.prepare(`INSERT INTO profiles
     (id,company_name,timezone,location,email,phone,created_at,updated_at) VALUES(?,?,?,?,?,?,?,?)`)
     insert.run(id, company_name, timezone, location, email, phone, created_at || now, updated_at || now);
+
+    const defaultHours = [{ day_of_week: 1, open_time: "09:00", close_time: "17:00" },
+    { day_of_week: 2, open_time: "09:00", close_time: "17:00" },
+    { day_of_week: 3, open_time: "09:00", close_time: "17:00" },
+    { day_of_week: 4, open_time: "09:00", close_time: "17:00" },
+    { day_of_week: 5, open_time: "09:00", close_time: "17:00" }
+    ];
+
+    for (const day of defaultHours) {
+        const business_hour_id = crypto.randomUUID();
+        db.prepare(`INSERT INTO business_hours(id,account_id,day_of_week,open_time,close_time) VALUES (?,?,?,?,?)`).run(business_hour_id, id, day.day_of_week, day.open_time, day.close_time);
+    }
     const profile = db.prepare(`SELECT * FROM profiles
         WHERE id=?`).get(id);
 
