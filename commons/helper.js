eaa6263a78352d2f7dd10f90b502ed8b99da0b59
KaushikTest <kaushik6990@gmail.com>
2025-08-24 18:50:25 +0530

    Update helper.js
    - Added slot generation helper methods
    - Added method to create profile request body

--- commons/helper.js
+++ commons/helper.js
@@ -2,6 +2,10 @@ import { DateTime } from "luxon";
 import request from 'supertest';
 import { TIME_FORMAT } from "../constants.js";
 import app from '../base/index.js';
+import { ProfileBuilder } from "../builders/profile-builder.js";
+import { Faker, en_IN, ta_IN, fakerEN_IN } from '@faker-js/faker';
+
+const faker = new Faker({ locale: [en_IN, ta_IN] });
 
 export async function bookEvent() {
 
@@ -55,4 +59,48 @@ export async function DeleteEvent(id) {
     let res = await request(app).delete(`/events/${id}`);
     return res;
 
+}
+
+export function generateSlots(start, end, slotSizeMinutes) {
+    const slots = [];
+    let slotStart = start;
+
+    while (slotStart.plus({ minutes: slotSizeMinutes }) <= end) {
+        slots.push({
+            start: slotStart,
+            end: slotStart.plus({ minutes: slotSizeMinutes })
+        });
+        slotStart = slotStart.plus({ minutes: slotSizeMinutes });
+    }
+
+    return slots;
+}
+
+export function isOverlapping(slot, unavailableIntervals) {
+    return unavailableIntervals.some(interval =>
+        slot.start < interval.end && slot.end > interval.start
+    );
+}
+
+
+export async function fetchProfile(id) {
+    let res = await request(app).get(`/profiles/${id}`);
+    return res;
+}
+
+export async function createProfile() {
+    let request_body = createProfileRequest();
+    let res = await request(app).post(`/profiles/create`)
+        .send(request_body);
+    return res;
+}
+
+export async function fetchHours(account_id) {
+    let res = await request(app).get(`/hours/${account_id}`);
+    return res;
+}
+
+
+function createProfileRequest() {
+    return new ProfileBuilder().setCompanyName(fakerEN_IN.company.name()).setTimezone(fakerEN_IN.location.timeZone()).setLocation(fakerEN_IN.location.streetAddress()).setEmail(fakerEN_IN.internet.email()).setPhone(fakerEN_IN.phone.number()).build();
 }
