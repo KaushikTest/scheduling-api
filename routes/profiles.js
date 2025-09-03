d71ebe3e623768b613629bf216caa696472ade81
KaushikTest <kaushik6990@gmail.com>
2025-09-03 06:12:03 +0530

    Update profiles.js
    Updated profile flow to create two profiles for ACCOUNT & STAFF, same for business hours, in this flow.

--- routes/profiles.js
+++ routes/profiles.js
@@ -9,11 +9,13 @@ const profileRouter = express.Router();
 profileRouter.post('/create', (req, res) => {
     const { company_name, timezone, location, email, phone, created_at, updated_at } = req.body;
 
-    const id = crypto.randomUUID();
+    const profile_id = crypto.randomUUID();
+    const profile_staff_id = crypto.randomUUID();
     const now = new Date().toISOString();
     const insert = db.prepare(`INSERT INTO profiles
-    (id,company_name,timezone,location,email,phone,created_at,updated_at) VALUES(?,?,?,?,?,?,?,?)`)
-    insert.run(id, company_name, timezone, location, email, phone, created_at || now, updated_at || now);
+    (id,company_name,timezone,location,email,phone,type,created_at,updated_at) VALUES(?,?,?,?,?,?,?,?,?)`)
+    insert.run(profile_id, company_name, timezone, location, email, phone, 'ACCOUNT', created_at || now, updated_at || now);
+    insert.run(profile_staff_id, company_name, timezone, location, email, phone, 'STAFF', created_at || now, updated_at || now);
 
     const defaultHours = [{ day_of_week: 1, open_time: "09:00", close_time: "17:00" },
     { day_of_week: 2, open_time: "09:00", close_time: "17:00" },
@@ -24,10 +26,12 @@ profileRouter.post('/create', (req, res) => {
 
     for (const day of defaultHours) {
         const business_hour_id = crypto.randomUUID();
-        db.prepare(`INSERT INTO business_hours(id,account_id,day_of_week,open_time,close_time) VALUES (?,?,?,?,?)`).run(business_hour_id, id, day.day_of_week, day.open_time, day.close_time);
+        const business_hour_staffId = crypto.randomUUID();
+        db.prepare(`INSERT INTO business_hours(id,account_id,day_of_week,type,open_time,close_time) VALUES (?,?,?,?,?,?)`).run(business_hour_id, profile_id, day.day_of_week, 'ACCOUNT', day.open_time, day.close_time);
+        db.prepare(`INSERT INTO business_hours(id,account_id,day_of_week,type,open_time,close_time) VALUES (?,?,?,?,?,?)`).run(business_hour_staffId, profile_staff_id, day.day_of_week, 'STAFF', day.open_time, day.close_time);
     }
     const profile = db.prepare(`SELECT * FROM profiles
-        WHERE id=?`).get(id);
+        WHERE id=?`).get(profile_id);
 
     return res.status(201).json({ message: PROFILE_CREATED, profile });
 
